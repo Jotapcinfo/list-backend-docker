c3f089a0403e66a748125f21c439d2e9d2cc074b
Jotapcinfo <prowlerurbanhc@gmail.com>
2023-05-09 13:24:09 -0300

    Project created

---
+++ src/main/java/br/com/fraseadotec/fraseadoteclist/FraseadoteclistApplication.java
@@ -0,0 +1,13 @@
+package br.com.fraseadotec.fraseadoteclist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraseadoteclistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraseadoteclistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fraseadotec/fraseadoteclist/FraseadoteclistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fraseadotec.fraseadoteclist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraseadoteclistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
