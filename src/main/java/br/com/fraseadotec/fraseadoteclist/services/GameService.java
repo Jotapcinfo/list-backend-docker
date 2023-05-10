a4bfbbb9830d2f324d6060413ca8913ce72b6819
Jotapcinfo <prowlerurbanhc@gmail.com>
2023-05-10 14:21:43 -0300

    GET game by id

--- src/main/java/br/com/fraseadotec/fraseadoteclist/services/GameService.java
+++ src/main/java/br/com/fraseadotec/fraseadoteclist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.fraseadotec.fraseadoteclist.dto.GameDTO;
 import br.com.fraseadotec.fraseadoteclist.dto.GameMinDTO;
 import br.com.fraseadotec.fraseadoteclist.entities.Game;
 import br.com.fraseadotec.fraseadoteclist.repositories.GameRepository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+		
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
